fa427c9ba146737f51eecd542f8a524aae918f98
Carly Pritchard <cpritch616@gmail.com>
2022-09-18 12:40:03 -0500

    configure workbox plugins and add CSS loaders and babel to webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,8 +3,8 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// TODO: Add and configure workbox plugins for a service worker and manifest file. DONE
+// TODO: Add CSS loaders and babel to webpack. DONE 
 
 module.exports = () => {
   return {
@@ -18,12 +18,59 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+        favicon: './favicon.ico',
+      }),
+      new WebpackPwaManifest({
+          name: 'Just Another Text Editor',
+          short_name: 'JATE',
+          description: 'A text editor',
+          background_color: '#7eb4e2',
+          theme_color: '#7eb4e2',
+          start_url: './',
+          publicPath: './',
+          icons: [
+            {
+              src: path.resolve('src/images/logo.png'),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join('assets', 'icons'),
+            },
+            {
+              src: path.resolve('src/images/logo.png'),
+              size: '1024x1024',
+              destination: path.join('assets', 'icons'),
+              purpose: 'maskable'
+            }
+          ],
+        }), 
+        new InjectManifest({
+          swSrc: './sw.js',
+          swDest: 'service-worker.js',
+        }), 
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
